c669c922d06d47669024fefa6548633d94b5bbec
Morten Delenk <morten@dark32.cf>
2016-12-11 12:29:28 +0100

    optimized 18

--- _0018.py
+++ _0018.py
@@ -20,33 +20,11 @@ triangle=[
     [63, 66, 4, 68, 89, 53, 67, 30, 73, 16, 69, 87, 40, 31],
     [4, 62, 98, 27, 23, 9, 70, 98, 73, 93, 38, 53, 60, 4, 23]]
 #NOTE: As there are only 16384 routes, it is possible to solve this problem by trying every route. However, Problem 67, is the same challenge with a triangle containing one-hundred rows; it cannot be solved by brute force, and requires a clever method! ;o)
-def greedy(line, row):
-    if line == 14:
-        return triangle[line][row]
+def bruteforce():
     global triangle
-    print("Value at",line,",",row,":",triangle[line][row])
-    left,right=triangle[line+1][row],triangle[line+1][row+1]
-    print("Value at left child:",left)
-    print("Value at right child:",right)
-    if left==right:
-        print("Peeking forward.")
-        if line == 13:
-            print("Neither of the children have children. choosing left.")
-            return triangle[line][row]+greedy(line+1,row)
-        left=max(triangle[line+2][row],triangle[line+2][row+1])
-        right=max(triangle[line+2][row+1],triangle[line+2][row+2])
-        print("Left child has children: ",(triangle[line+2][row],triangle[line+2][row+1]))
-        print("Right child has children: ",(triangle[line+2][row+1],triangle[line+2][row+1]))
-    if left > right:
-        print("Going left...")
-        return triangle[line][row]+greedy(line+1,row)
-    print("Going right...")
-    return triangle[line][row]+greedy(line+1,row+1)
-def bruteforce(line, row):
-    if line == 14:
-        return triangle[line][row]
-    val=max(bruteforce(line+1,row),bruteforce(line+1,row+1))
-    val+=triangle[line][row]
-    return val
-print(greedy(0,0))
-print(bruteforce(0,0))
+    for y in range(13,-1,-1): #last row is already inited
+        for x in range(y+1):
+            triangle[y][x]+=max(triangle[y+1][x],triangle[y+1][x+1])
+
+bruteforce()
+print(triangle[0][0])
